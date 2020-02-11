9592f4ea54961deede001bda3d01837fa42464ff
Marcin Dłubakowski <m.dlubakowski@gmail.com>
2020-02-11 16:18:06 +0100

    Init.

---
+++ main.cpp
@@ -0,0 +1,31 @@
+/*
+* This file is part of "Reddit Notifier".
+* 
+* Copyright 2020 Marcin Dłubakowski <marcin@dlubakowski.pl>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include <QApplication>
+#include "mainwindow.h"
+
+int main(int argc, char **argv)
+{
+    QApplication app(argc, argv);
+
+    rn::MainWindow mw;
+    mw.show();
+
+    return app.exec();
+}
---
+++ src/mainwindow.h
@@ -0,0 +1,37 @@
+/*
+* This file is part of "Reddit Notifier".
+* 
+* Copyright 2020 Marcin Dłubakowski <marcin@dlubakowski.pl>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include "ui_mainwindow.h"
+
+namespace rn {
+
+class MainWindow : public QMainWindow, public Ui::MainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow();
+};
+
+}
+
+#endif
